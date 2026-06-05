721e60dacc341bb69feef401eb018caa2207f0a5
HERAMBA KAKATI <herambakakati123@gmail.com>
2026-06-05 19:12:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 import streamlit as st
+
+# MUST BE FIRST STREAMLIT COMMAND
+st.set_page_config(
+    page_title="AI Medical Assistant",
+    page_icon="🩺",
+    layout="wide"
+)
+
 import tensorflow as tf
 import numpy as np
 import cv2
@@ -6,24 +14,9 @@ from PIL import Image
 import gdown
 import os
 
-# =====================================================
-# PAGE CONFIG (FIRST STREAMLIT COMMAND)
-# =====================================================
-
-st.set_page_config(
-    page_title="AI Medical Assistant",
-    page_icon="🩺",
-    layout="wide"
-)
-
-# =====================================================
-# MODEL DOWNLOAD
-# =====================================================
-
 MODEL_PATH = "vgg16_weights.weights.h5"
 
 if not os.path.exists(MODEL_PATH):
-
     url = "https://drive.google.com/uc?id=1eb-OCycqUVrRrM7BD39miq8Vbwef6DO9"
 
     with st.spinner("Downloading AI Model... Please wait."):
